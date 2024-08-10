dcddb50ec34f37610a7a200eb009ff27b2c3e41e
Umba joseph <Josephdjamba04@gmail.com>
2024-08-10 19:50:45 +0200

    Company class, Package added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,35 @@
 package com.example.demo;
 
+import com.example.demo.company.Company;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	@GetMapping
+
+	public List<Company> hello()
+	{
+		 return List.of(
+				 new Company(
+						 1L,
+						 "Dvt",
+						 "dvt@info.co.za",
+						 "0218298448",
+						 "dvd.com",
+						 "54 Century city boulevard"
+						 )
+		 );
+
 	}
 
 }
---
+++ src/main/java/com/example/demo/company/Company.java
@@ -0,0 +1,91 @@
+package com.example.demo.company;
+
+public class Company {
+    private Long id;
+    private String compName;
+    private String compEmail;
+    private String compNumber;
+    private String comWebsiteUrl;
+    private String compAdress;
+
+
+    public Company() {
+
+    }
+    public Company(Long id, String compName, String compEmail, String compNumber, String comWebsiteUrl, String compAdress) {
+        this.id = id;
+        this.compName = compName;
+        this.compEmail = compEmail;
+        this.compNumber = compNumber;
+        this.comWebsiteUrl = comWebsiteUrl;
+        this.compAdress = compAdress;
+    }
+
+    public Company(String compName, String compEmail, String compNumber, String comWebsiteUrl, String compAdress) {
+        this.compName = compName;
+        this.compEmail = compEmail;
+        this.compNumber = compNumber;
+        this.comWebsiteUrl = comWebsiteUrl;
+        this.compAdress = compAdress;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCompName() {
+        return compName;
+    }
+
+    public void setCompName(String compName) {
+        this.compName = compName;
+    }
+
+    public String getCompEmail() {
+        return compEmail;
+    }
+
+    public void setCompEmail(String compEmail) {
+        this.compEmail = compEmail;
+    }
+
+    public String getCompNumber() {
+        return compNumber;
+    }
+
+    public void setCompNumber(String compNumber) {
+        this.compNumber = compNumber;
+    }
+
+    public String getComWebsiteUrl() {
+        return comWebsiteUrl;
+    }
+
+    public void setComWebsiteUrl(String comWebsiteUrl) {
+        this.comWebsiteUrl = comWebsiteUrl;
+    }
+
+    public String getCompAdress() {
+        return compAdress;
+    }
+
+    public void setCompAdress(String compAdress) {
+        this.compAdress = compAdress;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "id=" + id +
+                ", compName='" + compName + '\'' +
+                ", compEmail='" + compEmail + '\'' +
+                ", compNumber=" + compNumber +
+                ", comWebsiteUrl='" + comWebsiteUrl + '\'' +
+                ", compAdress='" + compAdress + '\'' +
+                '}';
+    }
+}
